fa8da4d060233c5cbb8c685e29b54de303cf3492
Rusman Wahab <hwalyonglance@gmail.com>
2017-09-26 12:49:18 +0700

    add firebase module

--- src/client/app/modules/_dry/require.module.ts
+++ src/client/app/modules/_dry/require.module.ts
@@ -1,10 +1,16 @@
 import { BrowserAnimationsModule, NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { RouterModule } from "@angular/router";
 
+import { AngularFireModule }         from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule }     from 'angularfire2/auth';
+
 import { CdkModules, MatModules } from './material.module';
+
+import { firebaseConfig } from '../../../environments/firebase';
+
 export const RequireModule = [
 	BrowserAnimationsModule,
 	FlexLayoutModule,
@@ -12,6 +18,10 @@ export const RequireModule = [
 	NoopAnimationsModule,
 	ReactiveFormsModule,
 	RouterModule,
+
+	AngularFireModule.initializeApp(firebaseConfig),
+	AngularFireDatabaseModule,
+	AngularFireAuthModule,
 
 	...CdkModules,
 	...MatModules
