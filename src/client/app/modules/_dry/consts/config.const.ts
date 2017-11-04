487b7492f2e2faf60d8005c060daf20fb9fb1f32
Rusman Wahab <hwalyonglance@gmail.com>
2017-11-04 08:10:51 +0700

    gg

--- src/client/app/modules/_dry/consts/config.const.ts
+++ src/client/app/modules/_dry/consts/config.const.ts
@@ -1,3 +1,8 @@
-export const CONFIG = {
+declare var window: Window;
+const CONFIG = {
 	socket: 'http://localhost:4136'
 };
+if ( window.location.hostname !== 'localhost' ) {
+	CONFIG.socket = 'https://pp2-rental.herokuapp.com';
+}
+export { CONFIG }
